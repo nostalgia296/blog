f14478a2e75635e2686175a47c5a1ed1f76904b7
nostalgia296 <3570965752@qq.com>
2025-11-29 20:19:41 +0800

    feat.自定义base

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,10 +24,12 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-copy-button.js";
 
+const base = process.env.BASEURL || "/blog"
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://nostalgia296.github.io",
-	base: "/blog",
+	base: base,
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
